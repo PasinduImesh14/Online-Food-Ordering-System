2231ff4e402523c016bf3e4d9d34899742857849
Pasindu Imesh <pasinduimesh385@gmail.com>
2025-05-24 08:52:26 +0530

    update

--- src/controllers/restaurantController.js
+++ src/controllers/restaurantController.js
@@ -68,6 +68,37 @@ findRestaurantByName: async (req, res) => {
     }
 },
 
+getAllRestaurants: async (req, res) => {
+    try {
+        const restaurant = await restaurantService.getAllRestaurants();
+        res.status(200).json(restaurant);
+    } catch (error) {
+        res.status(500).json({error: "Internal server error"});
+    }
+},
 
+findResttaurantById: async (req, res) => {
+    try {
+        const {id} = req.params;
+        const restaurant = await restaurantService.findRestaurantById(id);
+        res.status(200).json(restaurant);
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(400).json({error: error.message});
+    } else{
+            res.status(500).json({error: "Internal server error"});
+        }
+}
+},
+
+addToFavourite: async (req, res) => {
+    try {
+        const {jwt} = req.headers;
+        const {id} = req.params;
+        
+    } catch (error) {
+        
+    }
+}
 
 };
