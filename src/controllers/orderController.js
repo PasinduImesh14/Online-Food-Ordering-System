103f3aafd6e3cddd7ec23f352d474bf10040678d
Pasindu Imesh <pasinduimesh385@gmail.com>
2025-06-02 15:48:49 +0530

    added orderController

---
+++ src/controllers/orderController.js
@@ -0,0 +1,34 @@
+const orderService = require("../services/order.service.js");
+const userService = require("../services/user.service.js");
+
+module.exports = {
+    createOrder: async (req, res) => {
+        try {
+            const order = req.body;
+            const user = req.user;
+            const paymentResponse = await orderService.createOrder(order, user);
+            res.status(200).json(paymentResponse);
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ error: error.message });
+            } else {
+                res.status(500).json({ error: "Internal server error" });
+            }
+        }
+    },
+
+    getAllUserOrders: async (req, res) => {
+        try {
+            user = req.user;
+            const userOrders = await orderService.getAllUserOrders(user._id);
+            res.status(200).json(userOrders);
+        } catch (error) {
+            if (error instanceof Error) {
+                res.status(400).json({ error: error.message });
+            } else {
+                res.status(500).json({ error: "Internal server error" });
+            }
+        }
+    },
+
+};
