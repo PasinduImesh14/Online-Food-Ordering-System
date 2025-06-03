9bbeb24634e673fee786783c6db6124c336df92a
Pasindu Imesh <pasinduimesh385@gmail.com>
2025-06-03 22:50:40 +0530

    added ingredient controller file

---
+++ src/controllers/ingredientController.js
@@ -0,0 +1,55 @@
+const ingredientService = require('../service/ingredient.service');
+
+module.exports = {
+    createIngredientCategory: async (req, res) => {
+        try {
+            const { name, restaurantId } = req.body;
+            const items = await ingredientService.createIngredientsCategory(name, restaurantId);
+            res.status(200).json(items);
+        } catch (error) {
+            res.status(400).json({ error: "Internal server error", message:error.message });
+        }
+    },
+
+    createIngredient: async (req, res) => {
+        try {
+            const { restaurantId, name, ingredientCategoryId } = req.body;
+            console.log(req.body);
+            const item = await ingredientService.createIngredientsItem(restaurantId, name, ingredientCategoryId);
+            res.status(200).json(item);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: true, message:error.message });
+        }
+    },
+
+    updateStoke: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const item = await ingredientService.updateStoke(id);
+            res.status(200).json(item);
+        } catch (error) {
+            res.status(500).json({ error: "Internal server error"});
+        }
+    },
+
+    restaurantsIngredient: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const items = await ingredientService.findRestaurantsIngredients(id);
+            res.status(200).json(items);
+        } catch (error) {
+            res.status(500).json({ error: "Internal server error"});
+        }
+    },
+
+    restaurantsIngredientCategory: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const items = await ingredientService.findIngredientsCategoryByRestaurantId(id);
+            res.status(200).json(items);
+        } catch (error) {
+            res.status(500).json({ error: "Internal server error" });
+        }
+    },
+};
